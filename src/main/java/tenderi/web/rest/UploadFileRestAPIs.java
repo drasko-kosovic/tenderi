4922edf6d0df55777bab682947efbc2eafdd3eb3
drasko <drasko.kosovic@montefarm.co.me>
2021-05-30 16:11:17 +0200

    upload

--- src/main/java/tenderi/web/rest/UploadFileRestAPIs.java
+++ src/main/java/tenderi/web/rest/UploadFileRestAPIs.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import tenderi.message.Message;
+import tenderi.Message;
 import tenderi.service.ExcelFileServices;
 import tenderi.utils.ExcelUtils;
 
