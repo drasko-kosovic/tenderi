f003fa405b5a0c1289e09456ce32e58e95d874e4
drasko-kosovic <drasko.kosovic@gmail.com>
2021-06-14 10:37:19 +0200

    fon name report Times New

--- src/main/webapp/app/entities/tenderi-home/list/tenderi-home.component.ts
+++ src/main/webapp/app/entities/tenderi-home/list/tenderi-home.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'jhi-tenderi-home',
   templateUrl: './tenderi-home.component.html',
   styleUrls: ['./tenderi-home.components.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TenderiHomeComponent implements OnInit {
   public sifra?: any;
+
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
