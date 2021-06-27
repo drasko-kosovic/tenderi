7960cb423aa67d493218cd7b6494cf93408eeb57
drasko-kosovic <drasko.kosovic@gmail.com>
2021-06-27 00:36:52 -0700

    dialog

--- src/main/webapp/app/entities/postupci/add/add.dialog.component.ts
+++ src/main/webapp/app/entities/postupci/add/add.dialog.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { IPostupci } from 'app/entities/postupci/postupci.model';
 import { PostupciService } from 'app/entities/postupci/service/postupci.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-add.dialog',
@@ -11,18 +12,25 @@ import { PostupciService } from 'app/entities/postupci/service/postupci.service'
   styleUrls: ['./add.dialog.component.scss'],
 })
 export class AddDialogComponent {
+  postupaks?: IPostupci[];
   constructor(
     public dialogRef: MatDialogRef<AddDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: IPostupci,
     public postupciService: PostupciService
   ) {}
-
+  loadAll(): void {
+    this.postupciService.query().subscribe((res: HttpResponse<IPostupci[]>) => {
+      this.postupaks = res.body ?? [];
+    });
+  }
   onNoClick(): void {
     this.dialogRef.close();
+    this.loadAll();
   }
 
   public confirmAdd(): void {
     this.postupciService.create(this.data).subscribe();
-    // window.location.reload();
+    window.location.reload();
+    // this.loadAll();
   }
 }
