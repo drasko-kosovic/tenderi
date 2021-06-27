32cc47fe079ba403ec376197959c175bebf7bea4
drasko-kosovic <drasko.kosovic@gmail.com>
2021-06-26 17:34:08 -0700

    word

--- src/main/java/tenderi/web/rest/PrvorangiraniPdf.java
+++ src/main/java/tenderi/web/rest/PrvorangiraniPdf.java
@@ -1,25 +1,29 @@
 package tenderi.web.rest;
 
-
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.export.SimpleDocxReportConfiguration;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import tenderi.domain.Anex;
+import tenderi.domain.Postupci;
 import tenderi.domain.Prvorangirani;
 import tenderi.repository.AnexRepository;
+import tenderi.repository.PostupciRepository;
 import tenderi.repository.PrvorangiraniRepository;
 import tenderi.repository.UgovorPdfRepository;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 @CrossOrigin(origins = ("*"))
 @RestController
@@ -29,25 +33,23 @@ public class PrvorangiraniPdf {
     @Autowired
     ApplicationContext context;
 
-
-
     @Autowired
     AnexRepository anexRepository;
 
+    @Autowired
+    PostupciRepository postupciRepository;
 
     @GetMapping(path = "/prvorangirani")
     @ResponseBody
-
-    public void getPdfUgovor(HttpServletResponse response, @RequestParam Integer sifraPostupka, @RequestParam Integer sifraPonude) throws Exception {
-
+    public void getPdfUgovor(HttpServletResponse response, @RequestParam Integer sifraPostupka, @RequestParam Integer sifraPonude)
+        throws Exception {
         Resource resource = context.getResource("classpath:reports/ReportAnex.jrxml");
         InputStream inputStream = resource.getInputStream();
         JasperReport report = JasperCompileManager.compileReport(inputStream);
 
         Map<String, Object> params = new HashMap<>();
 
-
-        List<Anex> anex= anexRepository.findBySifraPostupkaAndSifraPonude(sifraPostupka,sifraPonude);
+        List<Anex> anex = anexRepository.findBySifraPostupkaAndSifraPonude(sifraPostupka, sifraPonude);
 
         //Data source Set
         JRDataSource dataSource = new JRBeanCollectionDataSource(anex);
@@ -61,5 +63,41 @@ public class PrvorangiraniPdf {
         JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
     }
 
+    @GetMapping(path = "/postupci-docx")
+    @ResponseBody
+    public void getDocxUgovor(HttpServletResponse response) throws Exception {
+        Resource resource = context.getResource("classpath:reports/postupci.jrxml");
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+
+        Map<String, Object> params = new HashMap<>();
+
+        List<Postupci> postupci = postupciRepository.findAll();
+
+        //Data source Set
+        JRDataSource dataSource = new JRBeanCollectionDataSource(postupci);
+        params.put("datasource", dataSource);
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        JRDocxExporter export = new JRDocxExporter();
+        export.setExporterInput(new SimpleExporterInput(jasperPrint));
+        export.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+        response.setHeader("Content-Disposition", "attachment;filename=jasperfile.docx");
+        response.setContentType("application/octet-stream");
+        export.exportReport();
+    }
+    //        export.setExporterOutput(new SimpleOutputStreamExporterOutput(new File("C:\\Users\\drasko\\Downloads\\report.docx")));
+    //
+    //        SimpleDocxReportConfiguration config = new SimpleDocxReportConfiguration();
+    ////config.setFlexibleRowHeight(true); //Set desired configuration
+    //
+    //        export.setConfiguration(config);
+    //        export.exportReport();
+    //Make jasperPrint
+
+    //        //Media Type
+    //        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+    //        //Export PDF Stream
+    //        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
 
 }
